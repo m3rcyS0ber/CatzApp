91d5654585b45344a60f60372aa1c90e6de00e90
Keikem <keikem9501@yandex.ru>
2024-01-24 15:45:40 +0300

    Fix auto migration and add desciption

--- app/src/main/java/dev/keikem/catzapp/data/local/Database.kt
+++ app/src/main/java/dev/keikem/catzapp/data/local/Database.kt
@@ -1,12 +1,12 @@
 package dev.keikem.catzapp.data.local
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import dev.keikem.catzapp.data.local.dao.CatDao
 import dev.keikem.catzapp.data.local.dao.DogDao
 import dev.keikem.catzapp.data.local.entity.LocalCat
 import dev.keikem.catzapp.data.local.entity.LocalDog
-import dev.keikem.catzapp.data.local.entity.TestEntity
 
 
 //Обьект базы данных
@@ -14,9 +14,13 @@ import dev.keikem.catzapp.data.local.entity.TestEntity
     entities = [
         LocalCat::class,
         LocalDog::class,
-        TestEntity::class
     ],
+    //После добавления новой сущности - увеличиваем версию
     version = 1,
+    autoMigrations = [
+        //После добавления новой сущности - увеличиваем версию и добавляем авто миграции по образцу ниже
+        // AutoMigration(1, 2)
+    ],
     exportSchema = true
 )
 abstract class Database : RoomDatabase() {
@@ -24,4 +28,5 @@ abstract class Database : RoomDatabase() {
 
     abstract fun dogDao(): DogDao
 }
+
 
--- app/src/main/java/dev/keikem/catzapp/data/local/entity/TestEntity.kt
+++
@@ -1,9 +0,0 @@
-package dev.keikem.catzapp.data.local.entity
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "test_entity")
-data class TestEntity(
-    @PrimaryKey val id: String
-)
